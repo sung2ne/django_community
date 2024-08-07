1509978f64073d6739e17989f7b10958fa66adb4
정필성 <ibetter.kr@gmail.com>
2024-08-07 16:36:05 +0900

    기능 추가

--- mysite/board/templatetags/my_filter.py
+++ mysite/board/templatetags/my_filter.py
@@ -5,3 +5,13 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+@register.simple_tag(takes_context=True)
+def query_string(context, **kwargs):
+    request = context["request"]
+    updated = request.GET.copy()
+    
+    for key, value in kwargs.items():
+        updated[key] = value
+
+    return updated.urlencode()
--- mysite/board/views.py
+++ mysite/board/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.db.models import Q
 from board.models import Board
 
 # 목록, /board/
@@ -11,11 +12,32 @@ def board_list(request):
     # 목록
     b_list = Board.objects.all().order_by("-created_at")
     
+    # 검색
+    kw = request.GET.get("kw", "")
+    where = request.GET.get("where", "")
+    
+    if kw and where == "all":
+        b_list = b_list.filter(
+            Q(title__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) # 내용검색
+        ).distinct()
+    elif kw and where == "title":
+        b_list = b_list.filter(
+            Q(title__icontains=kw)
+        ).distinct()
+    elif kw and where == "content":
+        b_list = b_list.filter(
+            Q(content__icontains=kw)
+        ).distinct()
+    
     # 한페이지에 10개씩 보여주기
     paginator = Paginator(b_list, 10)
     page_obj = paginator.get_page(page)
     
     context = {
         "board_list": page_obj,
+        "kw": kw,
+        "where": where,
+        "page": page,
     }
     return render(request, "board/list.html", context)
