cd07027102dd1cd3d77266143b8f13a492f73d99
정필성 <ibetter.kr@gmail.com>
2024-08-07 11:47:41 +0900

    기능 추가

--- mysite/board/views.py
+++ mysite/board/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from board.models import Board
 
 # 목록, /board/
 def board_list(request):
-    return HttpResponse('/board/')
+    # 페이지 번호
+    page = request.GET.get("page", 1)
+    
+    # 목록
+    b_list = Board.objects.all().order_by("-created_at")
+    
+    # 한페이지에 10개씩 보여주기
+    paginator = Paginator(b_list, 10)
+    page_obj = paginator.get_page(page)
+    
+    context = {
+        "board_list": page_obj,
+    }
+    return render(request, "board/list.html", context)
