34af206ff5a9551ae535f6123a9a5b1a3ff2f07d
정필성 <ibetter.kr@gmail.com>
2024-08-09 15:51:55 +0900

    기능 수정

--- mysite/board/views.py
+++ mysite/board/views.py
@@ -7,6 +7,25 @@ from django.utils.safestring import mark_safe
 from django.contrib.auth import logout, authenticate, login
 from board.models import Board
 from board.forms import CreateForm
+import os 
+from openai import OpenAI
+
+
+# 요약하기
+def summary(content):
+    client = OpenAI(api_key = os.environ.get("OPENAI_API_KEY"))
+    
+    system_content = """당신은 고급 텍스트 요약 전문가입니다. 사용자로부터 입력받은 긴 텍스트를 간결하고 이해하기 쉬운 요약본으로 변환하는 것이 주된 역할입니다. 요약할 때는 중요한 정보를 빠뜨리지 않도록 주의하고, 핵심 내용만을 포함하도록 하십시오. 모든 요약은 중립적이고 명확하게 작성하며, 가능한 한 사용자가 원문에서 얻고자 하는 주요 메시지와 정보를 포함하도록 합니다. 필요에 따라 문체를 적절하게 유지하면서도 길이를 줄여야 합니다. 답변은 한국말로 하세요."""
+    
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": system_content},
+            {"role": "user", "content": content}
+        ]
+    )
+
+    return completion.choices[0].message.content
 
 # 목록, /board/
 def board_list(request):
