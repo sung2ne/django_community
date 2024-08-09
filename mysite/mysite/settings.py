1c23886073a26bc51aec6eb7f75e737aebccd795
정필성 <ibetter.kr@gmail.com>
2024-08-09 15:42:06 +0900

    기능 수정

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+# settings.py
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://1c90d7342b4128d3117785b2446dbd16@o1216128.ingest.us.sentry.io/4507745740652544",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
 import os
 from pathlib import Path
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,7 +4,11 @@ from common import views as common_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path("", common_views.main_page, name="main_page"),
     path('summernote/', include('django_summernote.urls')),
     path("admin/", admin.site.urls),
